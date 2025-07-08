2f0e924732fe2910aca17261004d70a0dd0173da
CD Cabrera <ccabrera@redhat.com>
2025-07-08 15:40:16 -0400

    refactor: CPUX-5560 move details underneath tooltip

--- src/Components/CreateImageWizard/steps/Registration/components/ActivationKeysList.tsx
+++ src/Components/CreateImageWizard/steps/Registration/components/ActivationKeysList.tsx
@@ -12,11 +12,15 @@ import {
   MenuToggle,
   TextInputGroup,
   TextInputGroupMain,
+  Popover,
+  Button,
 } from '@patternfly/react-core';
 import { useAddNotification } from '@redhat-cloud-services/frontend-components-notifications/hooks';
+import { InfoCircleIcon } from '@patternfly/react-icons';
 
 import ManageKeysButton from './ManageKeysButton';
 import PopoverActivation from './PopoverActivation';
+import ActivationKeyInformation from './ActivationKeyInformation';
 
 import { CDN_PROD_URL, CDN_STAGE_URL } from '../../../../../constants';
 import { useAppDispatch, useAppSelector } from '../../../../../store/hooks';
@@ -241,6 +245,11 @@ const ActivationKeysList = () => {
     </MenuToggle>
   );
 
+  const showViewDetails =
+    registrationType === 'register-now' ||
+    registrationType === 'register-now-insights' ||
+    registrationType === 'register-now-rhc';
+
   return (
     <>
       <FormGroup
@@ -250,17 +259,36 @@ const ActivationKeysList = () => {
           </>
         }
       >
-        <Select
-          isScrollable
-          isOpen={isOpen}
-          selected={activationKey}
-          onSelect={setActivationKey}
-          onOpenChange={handleToggle}
-          toggle={toggle}
-          shouldFocusFirstItemOnOpen={false}
-        >
-          <SelectList>{prepareSelectOptions()}</SelectList>
-        </Select>
+        <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+          <Select
+            isScrollable
+            isOpen={isOpen}
+            selected={activationKey}
+            onSelect={setActivationKey}
+            onOpenChange={handleToggle}
+            toggle={toggle}
+            shouldFocusFirstItemOnOpen={false}
+          >
+            <SelectList>{prepareSelectOptions()}</SelectList>
+          </Select>
+          {(!process.env.IS_ON_PREMISE && showViewDetails && activationKey) && (
+            <Popover
+              headerContent="Selected activation key"
+              position="right"
+              minWidth="30rem"
+              bodyContent={<ActivationKeyInformation />}
+              className="activation-key-details-popover"
+            >
+              <Button
+                variant="link"
+                icon={<InfoCircleIcon />}
+                iconPosition="left"
+              >
+                View details
+              </Button>
+            </Popover>
+          )}
+        </div>
         <Content>
           <Content>
             Create and manage activation keys on the <ManageKeysButton />
